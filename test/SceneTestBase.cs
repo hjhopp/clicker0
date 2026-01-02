e9c5eb0116fed6ee63e5026aeab0cdc9c5ad9c87
Hanno Hopp <hjhopp@proton.me>
2026-01-02 17:12:24 -0600

    Scene test class wrapper that allows us to take screenshots of tests that fail

---
+++ test/SceneTestBase.cs
@@ -0,0 +1,51 @@
+using Godot;
+using GdUnit4;
+using System;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+#pragma warning disable CA2200 // Re-throwing caught exception changes stack information
+
+[RequireGodotRuntime]
+public class SceneTestBase()
+{
+    public static async Task SceneTest(Func<Task> test, [CallerMemberName] string testName = "")
+    {
+        try
+        {
+            await test();
+        }
+        catch (Exception e)
+        {
+            await TakeScreenshot(testName);
+
+            throw e;
+        }
+    }
+
+    private static async Task TakeScreenshot(string testName = "")
+    {
+        var sceneTree = Engine.GetMainLoop() as SceneTree;
+
+        if (sceneTree == null)
+        {
+            return;
+        }
+
+        await sceneTree.ToSignal(sceneTree, SceneTree.SignalName.ProcessFrame);
+
+        var screenshot = sceneTree.Root.GetViewport().GetTexture().GetImage();
+
+        var now = DateTime.Now.ToString("yyMMdd_HHmmss");
+        var filename = $"{testName}_{now}.png";
+
+        try
+        {
+            screenshot.SavePng($"res://screenshots/{filename}");
+        }
+        catch
+        {
+            GD.Print("Unable to save screenshot");
+        }
+    }
+}
